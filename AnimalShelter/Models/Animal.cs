48eb429915299de2302bb1ee10f6c8cb9ed64a5b
DJ Zevenbergen <djzevenbergen@gmail.com>
2020-05-27 15:11:15 -0600

    add details

--- AnimalShelter/Models/Animal.cs
+++ AnimalShelter/Models/Animal.cs
@@ -5,24 +5,21 @@ namespace AnimalShelter.Models
   public class Animal
   {
     public int AnimalId { get; set; }
-
     public string Name { get; set; }
-    public AType AnimalType { get; set; }
+    public string AnimalType { get; set; }
     public string Breed { get; set; }
-
     public string Sex { get; set; }
-
     public DateTime DateAdmitted { get; set; }
 
   }
 
-  public enum AType
-  {
-    Dog,
-    Cat,
-    Fish,
-    Reptile,
-    Amphibian,
-    SmallMammal
-  }
+  // public enum AType
+  // {
+  //   Dog,
+  //   Cat,
+  //   Fish,
+  //   Reptile,
+  //   Amphibian,
+  //   SmallMammal
+  // }
 }
